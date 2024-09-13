aa6f31d94b86c352915ae383e12a903487210fd4
n0tx <rcandra91@msn.com>
2024-09-13 08:27:04 +0700

    add Product model

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "product")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name cannot be empty")
+    private String name;
+
+    @Min(value = 1, message = "Product price must be greater than 0")
+    private double price;
+
+    private String description;
+}
